ea3ef516a7438a06ab700adf748d5f30d9df1fed
Khoa <nguyenanhkhoablue@gmail.com>
2020-06-28 12:17:45 +0700

    Add A DeckOfCards Class
    Represent a deck of cards

--- main.py
+++ main.py
@@ -4,8 +4,28 @@ class ACard:
         self.suit = s
 
     def __repr__(self):
-        return '<Rank: {0!r}, Suit: {1!r}>'.format(self.rank, self.suit)
+        return '<[ACard] Rank: {0!r}, Suit: {1!r}>'.format(self.rank, self.suit)
 
 
+class DeckOfCards:
+    def __init__(self):
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ['Space', 'Club', 'Diamond', 'Heart']
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                card = ACard(rank, suit)
+                self.deck.append(card)
+
+    def __repr__(self):
+        return '<[DeckOfCards] deck: {0} cards>'.format(len(self.deck))
+
+
+"""Demo Section"""
 card0 = ACard('Three', 'Heart')
 print(card0)
+
+deck0 = DeckOfCards()
+print(deck0)
+for i in range(13):
+    print(deck0.deck[i])
